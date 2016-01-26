259b2f84f69baa4b789ce0dd02091341c1459dae
Joao Pedro Esteves <jayesteves@Joaos-MacBook-Pro.local>
2016-01-25 21:29:22 -0600

    added .gitignore . gitattributes

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Joao Pedro Esteves on 1/25/16.
+//  Copyright © 2016 Jay Esteves. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
